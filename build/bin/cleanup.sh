c3280c4e36c26f503fbcc48628e5b9421d1b41fc
Karl Isenberg <karl@mesosphere.io>
2016-01-20 13:05:40 -0800

    Add error checking to packer provisioning scripts
    - VBoxGuestAdditions gets an exception because OpenGL support module fails to install on the base centos box, as is

--- build/bin/cleanup.sh
+++ build/bin/cleanup.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
