489feffa65a7125651d58e8e7fc67a1941e389b2
Cody Maloney <cody@mesosphere.io>
2016-02-05 16:08:52 -0800

    Use faster provisioning

--- provision/bin/master.sh
+++ provision/bin/master.sh
@@ -5,4 +5,4 @@ set -o nounset
 set -o pipefail
 
 echo ">>> Installing DCOS master"
-curl --fail --location --silent --show-error --verbose http://boot.dcos/dcos_install.sh | bash -s -- master
+curl --fail --location --silent --show-error --verbose http://boot.dcos/dcos_install.sh | bash -s -- --no-block-dcos-setup master
