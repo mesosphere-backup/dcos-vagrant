7bd104d746b7ae4a7d5ea5f09dada3b6be9b9cd0
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2016-02-10 13:07:07 -0800

    Ensure we install ipset; copy release over.

--- provision/bin/minuteman.sh
+++ provision/bin/minuteman.sh
@@ -8,13 +8,17 @@ echo ">>> Setting path"
 export PATH=/opt/erlang-18.2.1/bin:$PATH
 echo $PATH
 
+echo ">>> Installing ipset"
+yum install --assumeyes --tolerant ipset
+
 echo ">>> Cloning minuteman"
-cd /opt
+cd /tmp
 git clone git@github.com:mesosphere/minuteman.git -b rebar3
 
-echo ">>> Compiling minuteman"
-cd /opt/minuteman
-make
-
-echo ">>> Building release"
+echo ">>> Compiling and building release of minuteman"
+cd /tmp/minuteman
 make rel
+
+echo ">>> Installing release"
+cp -r /tmp/minuteman/_build/default/rel/minuteman /opt/minuteman
+
