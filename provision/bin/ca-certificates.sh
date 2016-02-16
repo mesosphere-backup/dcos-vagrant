cdc9b57990c412db2e827080481b174a96aeedfb
Nick <nick@mesosphere.io>
2016-02-16 12:01:55 -0800

    Fancier cert install hook
    Check for presence of the needed files before creating the symlink, just in case

--- provision/bin/ca-certificates.sh
+++ provision/bin/ca-certificates.sh
@@ -4,6 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo ">>> Installing Certificate Authorities"
-# DCOS was compiled on ubuntu with certs in a different place!
-ln -s /etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
+# DCOS' libcurl expects Ubuntu's 'ca-certificates.crt', but CentOS names it 'ca-bundle.crt'.
+# Bridge this gap with a symlink.
+AVAILABLE_CERT_PATH=/etc/ssl/certs/ca-bundle.crt
+REQUIRED_CERT_PATH=/etc/ssl/certs/ca-certificates.crt
+if [ -f ${REQUIRED_CERT_PATH} ]; then
+    echo ">>> Skipping SSL cert symlink"
+else
+    echo ">>> Installing SSL cert symlink"
+    if [ ! -f ${AVAILABLE_CERT_PATH} ]; then
+        echo "!!! Missing expected certificate file at ${REQUIRED_CERT_PATH}. Exiting!"
+        exit 1
+    fi
+    ln -s ${AVAILABLE_CERT_PATH} ${REQUIRED_CERT_PATH}
+fi
