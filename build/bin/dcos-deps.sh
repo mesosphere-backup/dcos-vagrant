335cb225b33cd27d2adf20ec315ec91d150d27ea
Sargun Dhillon <sargun@sargun.me>
2016-03-25 13:11:21 -0700

    Add ipset package

--- build/bin/dcos-deps.sh
+++ build/bin/dcos-deps.sh
@@ -7,8 +7,8 @@ set -o pipefail
 echo ">>> Adding group [nogroup]"
 /usr/sbin/groupadd -f nogroup
 
-echo ">>> Installing packages (perl tar xz unzip curl bind-utils)"
-yum install --assumeyes --tolerant perl tar xz unzip curl bind-utils net-tools
+echo ">>> Installing packages (perl tar xz unzip curl bind-utils ipset)"
+yum install --assumeyes --tolerant perl tar xz unzip curl bind-utils net-tools ipset
 
 echo ">>> Creating ~/dcos"
 mkdir -p ~/dcos && cd ~/dcos
