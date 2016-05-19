0e664d4f8d9eb6395d4d276f5547bbe2efae30ce
Karl Isenberg <karl@mesosphere.io>
2016-05-19 08:29:21 -0700

    Use curl instead of yum installing netcat
    - current os box doesn't have DNS before DC/OS install

--- provision/bin/type-agent-public.sh
+++ provision/bin/type-agent-public.sh
@@ -4,9 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-yum install nc -y
-
-if ! nc -w 1 boot.dcos 80 < /dev/null; then
+if ! curl --fail --location --silent --show-error http://boot.dcos/dcos_install.sh > /dev/null; then
   echo ">>> Bootstrap machine unreachable - postponing DC/OS slave install"
   exit 0
 fi
