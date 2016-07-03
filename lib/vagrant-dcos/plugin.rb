6e21c315227d88c871cd423ab6f145ee280cd44f
Karl Isenberg <karl@mesosphere.io>
2016-07-02 23:13:20 -0700

    Delete unused provisioners

--- lib/vagrant-dcos/plugin.rb
+++ lib/vagrant-dcos/plugin.rb
@@ -3,20 +3,10 @@
 
 module VagrantPlugins
   module DCOS
-    VERSION = '0.1'.freeze
+    VERSION = '0.2'.freeze
 
     class Plugin < Vagrant.plugin(2)
       name 'dcos'
-
-      config :dcos_install, :provisioner do
-        require_relative 'provisioner_config'
-        ProvisionerConfig
-      end
-
-      provisioner :dcos_install do
-        require_relative 'provisioner'
-        Provisioner
-      end
 
       provisioner :dcos_ssh do
         require_relative 'ssh_provisioner'
