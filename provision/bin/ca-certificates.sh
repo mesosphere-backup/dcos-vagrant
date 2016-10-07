a8d339d404ff5df06edcce592d9d3f3b77696ad0
Karl Isenberg <karl@mesosphere.io>
2016-10-07 11:42:40 -0700

    Make provisioners more idempotent (for reprovisioning after errors)

--- provision/bin/ca-certificates.sh
+++ provision/bin/ca-certificates.sh
@@ -6,4 +6,6 @@ set -o pipefail
 
 echo ">>> Installing Certificate Authorities"
 # DC/OS was compiled on ubuntu with certs in a different place!
-ln -s /etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
+if [ "$(readlink /etc/ssl/certs/ca-certificates.crt)" != "/etc/ssl/certs/ca-bundle.crt" ]; then
+  ln -s /etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt
+fi
--- provision/bin/type-boot.sh
+++ provision/bin/type-boot.sh
@@ -4,17 +4,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo ">>> Starting zookeeper (for exhibitor bootstrap and quorum)"
-docker run -d -p 2181:2181 -p 2888:2888 -p 3888:3888 --restart=always jplock/zookeeper
+if [ "$(docker inspect -f '{{.State.Running}}' zookeeper-boot)" != "true" ]; then
+  echo ">>> Starting zookeeper (for exhibitor bootstrap and quorum)"
+  docker run -d --name=zookeeper-boot -p 2181:2181 -p 2888:2888 -p 3888:3888 --restart=always jplock/zookeeper
+else
+  echo ">>> Found zookeeper container running"
+fi
 
-echo ">>> Starting nginx (for distributing bootstrap artifacts to cluster)"
-docker run -d -v /var/tmp/dcos:/usr/share/nginx/html -p 80:80 --restart=always nginx
+if [ "$(docker inspect -f '{{.State.Running}}' ngnix-boot)" != "true" ]; then
+  echo ">>> Starting nginx (for distributing bootstrap artifacts to cluster)"
+  docker run -d --name=ngnix-boot -v /var/tmp/dcos:/usr/share/nginx/html -p 80:80 --restart=always nginx
+else
+  echo ">>> Found nginx container running"
+fi
 
 # Provide a local docker registry for testing purposes. Agents will also get
 # the boot node allowed as an insecure registry.
 if [ "${DCOS_PRIVATE_REGISTRY}" == "true" ]; then
-  echo ">>> Starting private docker registry"
-  docker run -d -p 5000:5000 --restart=always registry:2
+  if [ "$(docker inspect -f '{{.State.Running}}' registry-boot)" != "true" ]; then
+    echo ">>> Starting private docker registry"
+    docker run -d --name=registry-boot -p 5000:5000 --restart=always registry:2
+  else
+    echo ">>> Found registry container running"
+  fi
 fi
 
 if [ "${DCOS_JAVA_ENABLED:-false}" == "true" ]; then
