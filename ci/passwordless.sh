0af597aa29f9ee5f0d13cf68e4c55ab273ac9944
Karl Isenberg <karl@mesosphere.io>
2017-04-05 18:54:22 -0700

    Add ci/passwordless.sh
    - Automatically enables passwordless sudo for the current user
      for only the command executed by the vagrant-hostmanager plugin
    - Script requires (and enforces use only on) a Mac

---
+++ ci/passwordless.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ "${EUID}" -ne 0 ]; then
+  echo >&2 "Please run with sudo"
+  exit 1
+fi
+
+if ! echo "${OSTYPE}" | grep -q 'darwin'; then
+  echo >&2 "Incompatible OS - ${OSTYPE}"
+  exit 1
+fi
+
+USER_GROUP_ID="$(stat -f '%g' "${BASH_SOURCE}")"
+USER_GROUP="$(dscacheutil -q group -a gid ${USER_GROUP_ID} | grep name | cut -d ' ' -f 2)"
+echo >&2 "User Group: ${USER_GROUP}"
+
+ROOT_GROUP_ID="$(stat -f '%g' /etc/sudoers)"
+ROOT_GROUP="$(dscacheutil -q group -a gid ${ROOT_GROUP_ID} | grep name | cut -d ' ' -f 2)"
+echo >&2 "Root Group: ${ROOT_GROUP}"
+
+if [[ ! -d /etc/sudoers.d ]]; then
+  echo >&2 "Creating /etc/sudoers.d"
+  mkdir -p /etc/sudoers.d
+fi
+
+if ! cat /etc/sudoers | grep -q '#includedir /etc/sudoers.d'; then
+  echo >&2 "Updating /etc/sudoers to include /etc/sudoers.d"
+  tee -a /etc/sudoers << EOF
+#includedir /etc/sudoers.d
+EOF
+fi
+
+if ! cat /etc/sudoers.d/vagrant_hostmanager | grep -q 'VAGRANT_HOSTMANAGER_UPDATE'; then
+  echo >&2 "Creating /etc/sudoers.d/vagrant_hostmanager"
+  tee /etc/sudoers.d/vagrant_hostmanager << EOF
+Cmnd_Alias VAGRANT_HOSTMANAGER_UPDATE = /bin/cp ${HOME}/.vagrant.d/tmp/hosts.local /etc/hosts
+%${USER_GROUP} ALL=(root) NOPASSWD: VAGRANT_HOSTMANAGER_UPDATE
+EOF
+
+  echo >&2 "Updating permissions on /etc/sudoers.d/vagrant_hostmanager"
+  chmod 0440 /etc/sudoers.d/vagrant_hostmanager
+  chown root:${ROOT_GROUP} /etc/sudoers.d/vagrant_hostmanager
+fi
