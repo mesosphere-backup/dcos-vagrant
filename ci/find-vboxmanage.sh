9c5235531b6d0c782cdac0cbef2e7b6073af54f5
Karl Isenberg <karl@mesosphere.io>
2017-08-22 13:09:36 -0700

    Fix VBoxManage discovery on Linux Subsystem for Windows

--- ci/find-vboxmanage.sh
+++ ci/find-vboxmanage.sh
@@ -8,8 +8,11 @@ if hash VBoxManage 2>/dev/null; then
   # Bash on Mac/Linux should have VBoxManage in the PATH
   echo 'VBoxManage'
 elif [[ -f '/c/Program Files/Oracle/VirtualBox/VBoxManage.exe' ]]; then
-  # GitBash on Windows VirtualBox default install location
+  # GitBash on Windows - default VirtualBox install location
   echo '/c/Program Files/Oracle/VirtualBox/VBoxManage.exe'
+elif [[ -f '/mnt/c/Program Files/Oracle/VirtualBox/VBoxManage.exe' ]]; then
+  # Linux Subsystem for Windows - default VirtualBox install location
+  echo '/mnt/c/Program Files/Oracle/VirtualBox/VBoxManage.exe'
 else
   echo >&2 'ERROR: VBoxManage not found'
   exit 1
